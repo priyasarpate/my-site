571a138792f2a93037de0847000a412d2df1d60f
Gunpriya Sarpate <sarpatepriya@gmail.com>
2023-07-11 21:26:18 +0530

    made mobile responsive

--- src/componets/Aside.js
+++ src/componets/Aside.js
@@ -9,7 +9,7 @@ function Aside() {
       <aside className={styles.aside}>
         <Image src={profile} alt="" className={styles.profile} />
         <h1>Gunpriya Sarpate</h1>
-        <p>
+        <p className={styles.intro_text}>
           Welcome to my digital garden where I share what Im learning about
           shipping great products, becoming a better developer and growing a
           career in tech.
